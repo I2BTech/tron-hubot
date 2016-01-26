db7d338cfbc5e1638ec7a0a5d8285fc6760809b3
Jorge Epuñan <juanbrujo@gmail.com>
2016-01-26 18:37:02 -0300

    nuevo: alazar.js

---
+++ scripts/azar.js
@@ -0,0 +1,28 @@
+// Description:
+//   selecciona al azar de un conjunto separados por coma (,)
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   @tron al azar <algo1>,<algo2>,<algoN>,
+//
+// Author:
+//   @jorgeepunan
+
+
+
+function rand(items){
+  return items[~~(Math.random() * items.length)];
+}
+
+module.exports = function(robot) {
+  robot.respond(/al azar (.*)/i, function(res) {
+    var items = res.match[1].trim().split(",");
+    msg = "Al azar: `" + rand(items) + "` :dart: :ok_hand:";
+    res.send(msg);
+  });
+};
